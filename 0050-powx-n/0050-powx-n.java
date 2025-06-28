04cf6608fc95c88ceef469deab81474da280167e
Varun kumar reddy <varunkumardandu15@gmail.com>
2025-06-28 10:04:19 +0530

    Time: 0 ms (100%), Space: 43.1 MB (22.4%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,5 +1,17 @@
 class Solution {
+    public double pow(double x,long n){
+        if(n==0)return 1;
+        if(n==1)return x;
+        if(n%2==0){
+            return pow(x*x,n/2);
+        }
+        return x*pow(x,n-1);
+    }
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
+        long N=n;
+        if(N<0){
+            return 1.0/pow(x,-N);
+        }
+        return pow(x,N);
     }
 }
