71f6f9a79d946c765fbd58eed1665fd29e33a4ca
Varun kumar reddy <varunkumardandu15@gmail.com>
2025-07-01 10:24:23 +0530

    Time: 1 ms (99.9%), Space: 43.6 MB (61.11%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,19 +1,18 @@
 class Solution {
-    public void power(int index,int[] nums, List<Integer> cur, List<List<Integer>> ans) {
-            ans.add(new ArrayList<>(cur));
-            for(int i=index;i<nums.length;i++){
-                if(i>index&&nums[i]==nums[i-1])continue;
-                cur.add(nums[i]);
-                power(i+1, nums, cur, ans);
-                cur.remove(cur.size() - 1);
-        } 
+  public List<List<Integer>> subsetsWithDup(int[] nums) {
+    List<List<Integer>> ans = new ArrayList<>();
+    Arrays.sort(nums);  
+    pow(nums, 0, new ArrayList<>(), ans);
+    return ans;
+  }
+
+  private void pow(int[] nums, int s, List<Integer> path, List<List<Integer>> ans) {
+    ans.add(new ArrayList<>(path));
+    for (int i = s; i < nums.length; ++i) {
+      if (i > s && nums[i] == nums[i - 1]) continue;  
+      path.add(nums[i]);
+      pow(nums, i + 1, path, ans);
+      path.remove(path.size() - 1);
     }
-    public List<List<Integer>> subsetsWithDup(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> cur = new ArrayList<>();
-        Arrays.sort(nums);
-        power(0, nums, cur, ans);
-        return ans;
-    
-    }
+  }
 }
