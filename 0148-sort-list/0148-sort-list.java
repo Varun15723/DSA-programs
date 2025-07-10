c06bc150b4fa2c1afc638fe9ff20ac70f7814b3b
Varun kumar reddy <varunkumardandu15@gmail.com>
2025-07-10 10:50:49 +0530

    Time: 9 ms (96.23%), Space: 56.8 MB (61.6%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -8,23 +8,48 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
- import java.util.*;
 class Solution {
+    public ListNode midl(ListNode head){
+        ListNode slow=head;
+        ListNode fast=head.next;
+        while(fast!=null&&fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+    public ListNode merge(ListNode left,ListNode right){
+        ListNode dum=new ListNode(-1);
+        ListNode temp=dum;
+        while(left!=null && right!=null){
+            if(left.val<=right.val){
+                temp.next=left;
+                left=left.next;
+            }
+            else{
+                temp.next=right;
+                right=right.next;
+            }
+            temp=temp.next;
+        }
+        if(left!=null){
+            temp.next=left;
+        }
+        else{
+            temp.next=right;
+        }
+        return dum.next;
+    }
     public ListNode sortList(ListNode head) {
-         ListNode temp=head;
-         ArrayList<Integer> arr=new ArrayList<>();
-         while(temp!=null){
-            arr.add(temp.val);
-            temp=temp.next;
-         } 
-         Collections.sort(arr);
-         temp=head;
-         int i=0;
-         while(temp!=null){
-            temp.val=arr.get(i);
-            i++;
-            temp=temp.next;
-         }
-         return head;
+         if(head==null||head.next==null)return head;
+            ListNode mid = midl(head);
+            ListNode right = mid.next;
+            mid.next = null;
+            ListNode left = head;
+            left = sortList(left);
+            right = sortList(right);
+            return merge(left, right);
+
+         
     }
 }
