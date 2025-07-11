248d9cbff7277fae123ac31c36250cb42e79e03f
Varun kumar reddy <varunkumardandu15@gmail.com>
2025-07-11 10:47:37 +0530

    Time: 1 ms (100%), Space: 45.7 MB (97.42%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,17 +1,31 @@
-class Solution {
-    public int[] nextGreaterElements(int[] nums) {
-       int n=nums.length;
-       int[] res=new int[n];
-       for(int i=0;i<n;i++){
-        res[i]=-1;
-        for(int j=1;j<n;j++){
-            int in=(i+j)%n;
-            if(nums[in]>nums[i]){
-                res[i]=nums[in];
-                break;
-            }
+import java.util.Arrays;
+public class Solution {
+   
+    public int[] nextGreaterElements(int[] arr) {
+        int n = arr.length;
+        int[] result = new int[n];
+        int[] stack = new int[n];
+        int top = -1;
+
+        int i = n-1;
+        while(i>=0){
+            while(top!=-1 && stack[top]<=arr[i])
+                top--;
+            stack[++top] = arr[i];
+            i--;
         }
-       } 
-       return res;
+
+        int j = n-1;
+        while(j>=0){
+            while(top!=-1 && stack[top]<=arr[j])
+                top--;
+            if(top==-1)
+                result[j] = -1;
+            else
+                result[j] = stack[top];
+            stack[++top] = arr[j];
+            j--;
+        }
+        return result;
     }
 }
